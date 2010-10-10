e551820f971ebcbbc229a904168ccafde631efbc
compactcode <shanonmcquay@gmail.com>
2010-10-10 12:15:55 +1100

    add redirect rules for old blog content

---
+++ blog.rb
@@ -0,0 +1,24 @@
+require 'toto'
+require 'rack-rewrite'
+
+class Blog
+  def to_app
+    Rack::Builder.new {
+      use Rack::Static, :urls => ['/css', '/js', '/images', '/favicon.ico'], :root => 'public'
+      use Rack::CommonLogger
+
+      use Rack::Rewrite do
+        r301 %r{/index.php/(.*)}, 'http://compacted.wordpress.com/$1'
+        r301 %r{/2010/04/(.*)},   'http://compacted.wordpress.com/2010/04/$1'
+        r301 %r{/2009/(.*)},      'http://compacted.wordpress.com/2009/$1'
+      end
+      
+      run Toto::Server.new do
+        set :author, "shanon"
+        set :title, "compactcode"
+        set :url, "compactblog.heroku.com"
+        set :date, lambda {|now| now.strftime("%B #{now.day.ordinal} %Y") }
+      end
+    }.to_app
+  end
+end
---
+++ test/redirect_test.rb
@@ -0,0 +1,42 @@
+require "rubygems"
+require "test/unit"
+require "rack/test"
+require "blog"
+
+class RedirectTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Blog.new.to_app
+  end
+
+  def test_index_page_renders
+    get "/"
+    assert last_response.ok?
+  end
+
+  def test_old_wordpress_articles_are_redirected
+    get "/2009/09/introducing-compaction/"
+    follow_redirect!
+    assert_equal "http://compacted.wordpress.com/2009/09/introducing-compaction/", last_request.url
+  end
+
+  def test_all_old_wordpress_articles_are_redirected
+    get "/2010/04/google-collections-made-easier/"
+    follow_redirect!
+    assert_equal "http://compacted.wordpress.com/2010/04/google-collections-made-easier/", last_request.url
+  end
+
+  def test_older_wordpress_articles_are_redirected
+    get "/2009/09/introducing-compaction/"
+    follow_redirect!
+    assert_equal "http://compacted.wordpress.com/2009/09/introducing-compaction/", last_request.url
+  end
+
+  def test_very_old_wordpress_articles_are_redirected
+    get "/index.php/2009/09/introducing-compaction/"
+    follow_redirect!
+    assert_equal "http://compacted.wordpress.com/2009/09/introducing-compaction/", last_request.url
+  end
+
+end
